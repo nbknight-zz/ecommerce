a41d96847a3db834d64f3d0df0dea89f874201eb
Nick Knight <nicholas.b.knight@gmail.com>
2018-07-16 10:59:12 -0400

    Navbar and buttons-ish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'branding' => 'storefront#items_by_category'
+
+  get 'branding' => 'storefront#items_by_brand'
+
   resources :categories
   resources :products
 
-  root 'products#index'
+  root 'storefront#all_items'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
