6fc8e8b90984f8c44e9ce32c921de8dd56f4476a
Nick Knight <nicholas.b.knight@gmail.com>
2018-07-16 23:56:37 -0400

    Modified controller and navbar to allow for a Find by Brand dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :categories
+  before_action :brands
 
   def categories
   	@categories = Category.all
   end
   
+  def brands
+  	@brands = Product.pluck(:brand).sort.uniq
+  end
 end
--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -4,20 +4,37 @@ class StorefrontController < ApplicationController
   end
 
   def items_by_category
-  	@category = Category.find(params[:cat_id])
-  	@products = Product.all
-  	@products_by_cat = []
-
-  	@products.each do |product|
-  		if product.category.id == @category.id
-  			@products_by_cat.push(product)
-  		end
-  	end
-
-  	# @products = Product.where(category_id: params[:cat_id])
+    ######## Solution 1 ###############
   	# @category = Category.find(params[:cat_id])
+  	# @products = Product.all
+  	# @products_by_cat = []
+
+  	# @products.each do |product|
+  	# 	if product.category.id == @category.id
+  	# 		@products_by_cat.push(product)
+  	# 	end
+  	# end
+
+    ############Solution 2##############
+
+  	@products = Product.where(category_id: params[:cat_id])
+  	@category = Category.find(params[:cat_id])
   end
 
   def items_by_brand
+  ######## Solution 1 ###############
+    # @brand_name = params[:brand]
+    # @products = Product.all
+    # @products_by_brand = []
+
+    # @products.each do |product|
+    #   if product.brand == @brand_name
+    #     @products_by_cat.push(product)
+    #   end
+    # end
+    ############Solution 2##############
+    @products = Product.where(brand: params[:brand])
+    @brand = params[:brand]
+
   end
 end
