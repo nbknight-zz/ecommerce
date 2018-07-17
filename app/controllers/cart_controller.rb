13cba28e24203be78b39e75454fb1a752ea3b6ef
Nick Knight <nicholas.b.knight@gmail.com>
2018-07-17 15:00:21 -0400

    Added totals and taxes to checkout. Fixed nil error when quantity 0.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,10 +2,17 @@ class CartController < ApplicationController
 	before_action :authenticate_user!, only: [:checkout]
 
   def add_to_cart
-  	line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
-  	line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
 
-  	redirect_to root_url
+    if params[:quantity].blank?
+      redirect_to root_url
+
+    else
+    	line_item = LineItem.create(product_id: params[:product_id], quantity: params[:quantity])
+    	line_item.update(line_item_total: (line_item.quantity * line_item.product.price))
+
+    	redirect_to root_url
+    end
+    
   end
 
   def delete_from_cart
@@ -13,7 +20,7 @@ class CartController < ApplicationController
     line_item = LineItem.find(params[:line_item_id])
     line_item.destroy
 
-    redirect_to root_url
+    redirect_to view_order_path
 
   end
 
@@ -22,5 +29,37 @@ class CartController < ApplicationController
   end
 
   def checkout
+    line_items = LineItem.all
+
+    if line_items.empty?
+
+      redirect_to root_url
+    else
+
+    @order = Order.create(user_id: current_user.id, subtotal: 0)
+
+    line_items.each do |line_item|
+
+      line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
+
+      if @order.order_items[line_item.product_id].nil?
+        @order.order_items[line_item.product_id] = line_item.quantity
+      else
+        @order.order_items[line_item.product_id] += line_item.quantity
+      end
+
+      @order.subtotal += line_item.line_item_total
+
+    end
+
+    @order.save
+
+    @order.update(sales_tax: (@order.subtotal * 0.08))
+    @order.update(grand_total: (@order.sales_tax + @order.subtotal))
+
+    line_items.destroy_all
+
+  end
+
   end
 end
